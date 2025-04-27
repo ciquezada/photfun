923a9c1433e4f40b7047a669eb56f51ec7e41636
ciquezada <ciquezada@uc.cl>
2025-04-27 04:41:56 -0400

    docker fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='photfun',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     install_requires=[
         "astropy==7.0.1",
